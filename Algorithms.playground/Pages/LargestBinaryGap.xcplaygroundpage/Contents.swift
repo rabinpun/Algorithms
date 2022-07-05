fcd394a521502e18220a6893620344fc4bf614d1
rabin.pun <rabin.pun@ebpearls.com>
2022-07-05 08:07:32 +0545

    largest binary gap

---
+++ Algorithms.playground/Pages/LargestBinaryGap.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+
+//MARK: Solution
+private struct Solution {
+    
+    static func binaryGap(_ N: Int) -> Int {
+        let binaryRep = String(N, radix: 2)
+        var bitIndices = [Int]()
+        var largestGap = 0
+        
+        for (i, char) in binaryRep.enumerated() where char == "1" {
+            bitIndices.append(i)
+        }
+        
+        for i in 0...bitIndices.count - 2 {
+            let gap = bitIndices[i+1] - bitIndices[i] - 1
+            largestGap = max(gap,largestGap)
+        }
+        
+        return largestGap
+    }
+    
+}
+
+//MARK: Unit tests
+
+import XCTest
+class SolutionTests: XCTestCase {
+    
+    func testBinaryGap() {
+        XCTAssert(Solution.binaryGap(1041) == 5)
+        XCTAssert(Solution.binaryGap(15) == 0)
+        XCTAssert(Solution.binaryGap(32) == 0)
+        XCTAssert(Solution.binaryGap(529) == 4)
+    }
+}
+
+SolutionTests().run()
