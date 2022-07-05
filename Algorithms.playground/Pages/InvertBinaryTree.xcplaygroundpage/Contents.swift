1e91b0a7c89a4a0baab7a8aac54442cf303c4668
rabin.pun <rabin.pun@ebpearls.com>
2022-07-05 17:58:01 +0545

    invert binary tree without recursion

---
+++ Algorithms.playground/Pages/InvertBinaryTree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: [Next](@next)
+class Node {
+    var value: Int?
+    var left: Node?
+    var right: Node?
+}
+
+//Rules to invert the tree
+//1. Left node of the original node becomes the root node
+//2. Root node becomes left node of the right node of the new root node
+//3. Right node of the original node becomes the left node of the new root node
+
+struct Solution {
+    
+    static func invertBinaryTreeWithoutRecursion(_ root: Node?) -> Node? {
+        var previousNode: Node?
+        var nextNode: Node?
+        var tempNode: Node?
+        var currentNode = root
+        
+        while currentNode != nil {
+            
+            nextNode = currentNode?.left
+            
+            //swapping
+            
+            //swapping current node's left with previous node right
+            currentNode?.left = tempNode
+            tempNode = currentNode?.right
+            
+            //swapping current node's right with previous node left
+            currentNode?.right = previousNode
+            
+            previousNode = currentNode
+            currentNode = nextNode
+        }
+        
+        return previousNode
+    }
+}
