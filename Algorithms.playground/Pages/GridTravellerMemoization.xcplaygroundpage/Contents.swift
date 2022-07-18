c8e3223fd3a42161767474f42ed1dfe643c53a41
rabin.pun <rabin.pun@ebpearls.com>
2022-07-18 11:51:36 +0545

    ways to travel a grid memoization

--- Algorithms.playground/Pages/GridTravellerMemoization.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/GridTravellerMemoization.xcplaygroundpage/Contents.swift
@@ -2,6 +2,31 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+struct Solution {
+    
+    func solution(_ row: Int, _ column: Int, memo: inout [String: Int]) -> Int {
+        let key = "\(row),\(column)"
+        guard !memo.keys.contains(key) else { return memo[key]! }
+        guard row != 0 || column != 0 else { return 0 }// 0 ways to travel grid
+        guard row != 1 , column != 1 else { return 1 }// only 1 way to travel 1 * 1 grid
+        
+        memo[key] = solution(row - 1, column, memo: &memo) + solution(row, column - 1, memo: &memo)
+        return memo[key]!
+    }
+    
+}
 
-//: [Next](@next)
+
+
+
+import XCTest
+
+class SolutionTests: XCTestCase {
+
+    func test() {
+        
+        var memo: [String: Int] = [:]
+        XCTAssert( Solution().solution(18, 18, memo: &memo) == 2333606220)
+    }
+}
+SolutionTests.defaultTestSuite.run()
