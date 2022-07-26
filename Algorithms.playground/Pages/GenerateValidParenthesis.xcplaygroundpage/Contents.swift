1f73735a645cfc59316ebdce5a456a586563aae4
rabin.pun <rabin.pun@ebpearls.com>
2022-07-26 13:09:34 +0545

    generate valid parenthesis

--- Algorithms.playground/Pages/GenerateValidParenthesis.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/GenerateValidParenthesis.xcplaygroundpage/Contents.swift
@@ -2,6 +2,41 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+struct Solution {
+    
+    func soln(n: Int) -> [String] {
+        var solution = [String]()
+        balancedBracket(&solution,"", n, 0, 0)
+        return solution
+    }
 
-//: [Next](@next)
+    func balancedBracket(_ solution: inout [String],_ result: String, _ size: Int, _ openParenthesisCount: Int, _ closedParenthesisCount: Int)
+        {
+            if (close == size)
+            {
+                solution.append(result)
+                return
+            }
+            if (open < size)
+            {
+                self.balancedBracket(&solution, result + "(", size, openParenthesisCount + 1, closedParenthesisCount)
+            }
+            if (open > close)
+            {
+                self.balancedBracket(&solution, result + ")", size, openParenthesisCount, closedParenthesisCount + 1);
+            }
+        }
+}
+
+//MARK: Unit tests
+
+import XCTest
+
+class SolutionTests: XCTestCase {
+
+    func test() {
+        debugPrint(Solution().soln(n: 3))
+    }
+}
+
+SolutionTests.defaultTestSuite.run()
